ab178210b72218d42b3b41170d1f9524bbdf38ec
maxyymmm <120425774+maxyymmm@users.noreply.github.com>
2023-08-16 17:49:48 +0200

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from food import Food
+ALIGMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Food):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGMENT, font=FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
